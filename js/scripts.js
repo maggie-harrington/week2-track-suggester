c0e3ac8968d3acc1fd70c5f03a6048cb69da68d8
Maggie Harrington <maggie.harrington@gmail.com>
2017-01-13 13:45:47 -0800

    expand quiz functionality to 6 possible results and add citation link

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,30 @@ $(document).ready(function(){
   $("form#interestsquiz").submit(function(event){
     var question1result = parseInt($("input:radio[name=question1]:checked").val());
     var question2result = parseInt($("input:radio[name=question2]:checked").val());
+    var question3result = parseInt($("input:radio[name=question3]:checked").val());
+    var question4result = parseInt($("input:radio[name=question4]:checked").val());
+    var question5result = parseInt($("input:radio[name=question5]:checked").val());
 
     $("#interestsquiz").hide();
 
     if (question1result === 1) {
       $("#resultA").show();
+      $("#citation").show();
     } else if (question2result === 1){
       $("#resultB").show();
-    } else {
+      $("#citation").show();
+    } else if (question3result === 1){
       $("#resultC").show();
+      $("#citation").show();
+    } else if (question4result === 1){
+      $("#resultD").show();
+      $("#citation").show();
+    } else if (question5result === 1){
+      $("#resultE").show();
+      $("#citation").show();
+    } else {
+      $("#resultF").show();
+      $("#citation").show();
     }
 
     event.preventDefault();
